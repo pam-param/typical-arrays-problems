f0b99c0b04a98ecc169322813dfbf51ae3199111
pam-param <tamarova.irina@gmail.com>
2020-09-30 17:01:53 +0300

    feat: add solution for typical arrays problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min (array = []) {
+  if (validateArray(array)) {
+    return Math.min(...array);
+  }
 
-exports.max = function max (array) {
   return 0;
-}
+};
 
-exports.avg = function avg (array) {
+exports.max = function max (array = []) {
+  if (validateArray(array)) {
+    return Math.max(...array);
+  }
+
   return 0;
+};
+
+exports.avg = function avg (array = []) {
+  if (validateArray(array)) {
+    const sum = array.reduce((sum, currentValue) => sum + currentValue, 0);
+    return sum / array.length;
+  }
+
+  return 0;
+};
+
+function validateArray(array) {
+  return array.length !== 0;
 }
